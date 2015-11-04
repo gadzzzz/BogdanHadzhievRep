1c8ecfeb8697fec2849c7b1b203391ce000ac3cb
gadzzzz <bogdanhadzhiev@yahoo.com>
2015-11-04 14:54:40 +0300

    Build 0.1

--- sqlcommand/src/main/java/org/sqlcommand/QueryInjector.java
+++ sqlcommand/src/main/java/org/sqlcommand/QueryInjector.java
@@ -10,11 +10,11 @@ public class QueryInjector {
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-	public String executeQuery(String query){
+	public String executeQuery(String query) throws Exception{
 		List<Map<String, Object>> result = jdbcTemplate.queryForList(query.substring(0,query.length()-1));
 		return result.toString();
 	}
-	public void executeUpdate(String query){
+	public void executeUpdate(String query) throws Exception{
 		jdbcTemplate.execute(query.substring(0,query.length()-1));
 	}
 }
--- sqlcommand/src/main/java/org/sqlcommand/SqlCommand.java
+++ sqlcommand/src/main/java/org/sqlcommand/SqlCommand.java
@@ -3,11 +3,12 @@ package org.sqlcommand;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
-
+import org.apache.log4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SqlCommand {
+	final static Logger LOGGER = Logger.getLogger(SqlCommand.class);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -21,18 +22,28 @@ public class SqlCommand {
 			String tmpQuery = in.nextLine();
 			query=query+" "+tmpQuery;
 			for(int i=0;i<tmpQuery.length();i++)
-				if(tmpQuery.charAt(i)==';'){
-					if(query.substring(0,7).toLowerCase().trim().equals("create"))
-						injector.executeUpdate(query.trim());
-					if(query.substring(0,7).toLowerCase().trim().equals("update"))
-						injector.executeUpdate(query.trim());
-					if(query.substring(0,7).toLowerCase().trim().equals("insert"))
-						injector.executeUpdate(query.trim());
-					if(query.substring(0,7).toLowerCase().trim().equals("delete"))
-						injector.executeUpdate(query.trim());
-					if(query.substring(0,7).toLowerCase().trim().equals("select"))
-						System.out.println(injector.executeQuery(query.trim()));
+				try{
+					if(tmpQuery.charAt(i)==';'){
+						LOGGER.info(query.trim());
+						if(query.substring(0,7).toLowerCase().trim().equals("create"))
+								injector.executeUpdate(query.trim());
+						if(query.substring(0,7).toLowerCase().trim().equals("update"))
+							injector.executeUpdate(query.trim());
+						if(query.substring(0,7).toLowerCase().trim().equals("insert"))
+							injector.executeUpdate(query.trim());
+						if(query.substring(0,7).toLowerCase().trim().equals("delete"))
+							injector.executeUpdate(query.trim());
+						if(query.substring(0,7).toLowerCase().trim().equals("select")){
+							String result = injector.executeQuery(query.trim());
+							System.out.println(result);
+							LOGGER.info(result);
+						}
+						query="";
+					}
+				}catch(Exception e){
+					System.out.println(e);
 					query="";
+					LOGGER.error("Error in SQL" + e);
 				}
 		}
 		context.close();
